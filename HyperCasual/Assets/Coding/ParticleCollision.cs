dd7d35157a35f991ac3b2b40c2ce2d48de2f10c7
koolgamer13 <46666733+koolgamer13@users.noreply.github.com>
2020-01-01 15:54:54 -0500

    Uploading Current Status of Game to Github

---
+++ HyperCasual/Assets/Coding/ParticleCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCollision : MonoBehaviour
+{
+
+ 
+    public ParticleSystem effects;
+    // Start is called before the first frame update
+   private void Start()
+    {
+        effects.GetComponent<ParticleSystem>();
+        
+    }
+
+  
+    
+    private void OnCollisionEnter2d(Collision sparkle)
+    {
+        if (sparkle.gameObject.tag == "Player") {
+
+            effects.Play();
+
+        }
+    }
+}
